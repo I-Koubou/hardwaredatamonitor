a84a1f4ab06d5c3e41aa83c3f4f46cbe9857267b
ibui2628 (I-Koubou) <92432744+ibui2628@users.noreply.github.com>
2022-04-27 19:51:28 +0900

    Update hardwaredatamonitor.py

--- hardwaredatamonitor.py
+++ hardwaredatamonitor.py
@@ -7,11 +7,17 @@ import psutil
 
 sg.theme('Default1')
 
-layout =[[sg.Frame('CPU使用率%', [[sg.MLine(key='-cput-'+sg.WRITE_ONLY_KEY,  size=(8,3))]]),sg.Frame('メモリ使用率%', [[sg.MLine(key='-memt-'+sg.WRITE_ONLY_KEY,  size=(8,3))]])]],[sg.MenuBar([['詳細',['ハードウェア情報','バージョン情報','終了']]], key='menu')]
+frame_cpu = sg.Frame('CPU使用率%', [
+[sg.MLine(key='-cput-'+sg.WRITE_ONLY_KEY,size=(8,3))]
+])
+
+frame_rem = sg.Frame('メモリ使用率%', [
+    [sg.MLine(key='-memt-'+sg.WRITE_ONLY_KEY,size=(8,3))]
+])
+
+layout =[frame_cpu,frame_rem],[sg.MenuBar([['詳細',['ハードウェア情報','バージョン情報','終了']]], key='menu')]
 
 window = sg.Window('パフォーマンス', layout, finalize=True,)
-
-
 
 cpu = 0
 mem = 0
@@ -27,7 +33,7 @@ while True:
         desk=psutil.disk_usage(path='/').percent
         desapa=psutil.disk_partitions()
         netwook=psutil.net_if_addrs()
-        sg.popup_ok("コア数",cpucor,"論理コア数",cpucor2,"ディスク使用率%",desk,"------------------------------\n","ディスクパーティション",desapa,"ネットワーク情報(自分も何なのか分からない)",netwook ,title = "ハードウェア情報",font=(12))
+        sg.popup_ok("コア数",cpucor,"論理コア数",cpucor2,"ディスク使用率%",desk,"ディスクパーティション",desapa,"ネットワーク情報(自分も何なのか分からない)",netwook ,title = "ハードウェア情報",font=(12))
     if event == values['menu']=='バージョン情報':
         sg.popup_ok("バージョン: 1.0.0",title ="バージョン情報",font=(12))
     mem = psutil.virtual_memory() 
